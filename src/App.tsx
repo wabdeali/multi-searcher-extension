be807a92e55fa1966ea71b6cffadfedc916a7409
Abdeali Waseef <wabeali@gmail.com>
2024-09-21 12:21:10 +0530

    initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TypographyH1 } from "./components/Typography";
+
+function App() {
+  return (
+    <main>
+      <TypographyH1>Hello world!</TypographyH1>
+    </main>
+  );
+}
+
+export default App;
